7ef0ba59504c70fa30c05ec428a012417cb8fce4
Sri Harsha Konatham <harshakon8@gmail.com>
2025-08-11 23:28:07 +0530

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Dashboard from './pages/Dashboard'
+
+export default function App(){
+  return <div className='min-h-screen p-4 max-w-3xl mx-auto'>
+    <Dashboard />
+  </div>
+}
